7fd97d24ac85f370af267f4e70912b4351888348
Naveen Rai <rai.naveen75@gmail.com>
2024-05-23 13:31:04 +0530

    some change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const express =require("express");
-const mongoose =require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const storyRoute = require("./routes/story");
 const verifyToken = require("./middlewares/verifyToken");
@@ -9,36 +9,44 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://story-backend-rho.vercel.app/"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 mongoose
-      .connect(process.env.MONGODB_URI || "mongodb+srv://rainaveen75:dYYDMRZYIWNvBD90@cluster0.odqezql.mongodb.net/?retryWrites=true&w=majority&appName=cluster0")
-      .then(()=> console.log("DB COnnected!"))
-      .catch((error) => console.log("Db failed to connect", error));
+  .connect(
+    process.env.MONGODB_URI ||
+      "mongodb+srv://rainaveen75:dYYDMRZYIWNvBD90@cluster0.odqezql.mongodb.net/?retryWrites=true&w=majority&appName=cluster0"
+  )
+  .then(() => console.log("DB COnnected!"))
+  .catch((error) => console.log("Db failed to connect", error));
 
-
-app.get("/api/health", (req,res) => {
-    res.json({
-        service: "Backend server",
-        status: "active",
-        time: new Date(),
-    });
+app.get("/api/health", (req, res) => {
+  res.json({
+    service: "Backend server",
+    status: "active",
+    time: new Date(),
+  });
 });
-  
+
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/story", storyRoute);
 
 app.use("*", (req, res) => {
-    res.status(404).json({ errorMessage: "Route not found!" });
+  res.status(404).json({ errorMessage: "Route not found!" });
 });
 
 app.use((error, req, res, next) => {
-    console.log(error);
-    res.status(500).json({ errorMessage: "Something went wrong!" });
+  console.log(error);
+  res.status(500).json({ errorMessage: "Something went wrong!" });
 });
 
 const PORT = process.env.port || 6000;
 
 app.listen(PORT, () => {
-    console.log(`Backend server running at port ${PORT}`);
+  console.log(`Backend server running at port ${PORT}`);
 });
