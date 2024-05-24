a8bddb3e1a2ded4bbcaeddbc92d8016f1d4903e0
Naveen Rai <84222473+nav758@users.noreply.github.com>
2024-05-25 00:29:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://story-backend-rho.vercel.app"],
+    origin: ["http://localhost:5174/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
